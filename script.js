e1a5856a956a35550e272aa83d9619521ce3e24f
DenverCoder1 <jonah@freshidea.com>
2021-01-06 12:18:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -39,6 +39,68 @@ let weather = {
   },
 };
 
+let geocode = {
+  reverseGeocode: function (latitude, longitude) {
+    var apikey = "2cad6041208649f7bacaf9013ad4ec2e";
+
+    var api_url = "https://api.opencagedata.com/geocode/v1/json";
+
+    var request_url =
+      api_url +
+      "?" +
+      "key=" +
+      apikey +
+      "&q=" +
+      encodeURIComponent(latitude + "," + longitude) +
+      "&pretty=1" +
+      "&no_annotations=1";
+
+    // see full list of required and optional parameters:
+    // https://opencagedata.com/api#forward
+
+    var request = new XMLHttpRequest();
+    request.open("GET", request_url, true);
+
+    request.onload = function () {
+      // see full list of possible response codes:
+      // https://opencagedata.com/api#codes
+
+      if (request.status == 200) {
+        // Success!
+        var data = JSON.parse(request.responseText);
+        weather.fetchWeather(data.results[0].components.city);
+        console.log(data.results[0].components.city)
+      } else if (request.status <= 500) {
+        // We reached our target server, but it returned an error
+
+        console.log("unable to geocode! Response code: " + request.status);
+        var data = JSON.parse(request.responseText);
+        console.log("error msg: " + data.status.message);
+      } else {
+        console.log("server error");
+      }
+    };
+
+    request.onerror = function () {
+      // There was a connection error of some sort
+      console.log("unable to connect to server");
+    };
+
+    request.send(); // make the request
+  },
+  getLocation: function() {
+    function success (data) {
+      geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+    }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, console.error);
+    }
+    else {
+      weather.fetchWeather("Denver");
+    }
+  }
+};
+
 document.querySelector(".search button").addEventListener("click", function () {
   weather.search();
 });
@@ -51,4 +113,4 @@ document
     }
   });
 
-weather.fetchWeather("Denver");
+geocode.getLocation();
